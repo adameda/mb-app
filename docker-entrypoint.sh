c98b5fca62f199d157fad87f2c70559717412d14
adameda <adamhalladja@yahoo.fr>
2026-02-04 09:38:36 +0100

    chore: ajouter .railwayignore et nettoyer fichiers inutilisés

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Utiliser le PORT de Railway ou 5000 par défaut
-PORT="${PORT:-5000}"
-
-# Lancer Gunicorn avec exec pour recevoir les signaux proprement
-exec gunicorn --bind "0.0.0.0:${PORT}" --workers 2 run:app
